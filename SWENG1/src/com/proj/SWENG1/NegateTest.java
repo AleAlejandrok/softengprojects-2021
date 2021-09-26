ba1793c5d9e9fb3c62b96c8f1959d9b9f9591c30
Thecodebilly <N01401127@unf.edu>
2021-09-26 09:51:35 -0400

    Added single test case for negate
    could be used as a template for the other tests

---
+++ SWENG1/src/com/proj/SWENG1/NegateTest.java
@@ -0,0 +1,19 @@
+package com.proj.SWENG1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NegateTest {
+
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
