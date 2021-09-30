1ead0057f17ff76c79dfc4a886bbe93d2e0d21a0
alejandrokatz <alejandrokatz@hackbookpro.attlocal.net>
2021-09-30 16:40:44 -0400

    RM NegateTest Added Vector3DTest with stubs

---
+++ SWENG1/src/com/proj/SWENG1/Vector3DTest.java
@@ -0,0 +1,54 @@
+package com.proj.SWENG1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Vector3DTest {
+
+	@Test
+	void testVector3D() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testScale() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSubtract() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testNegate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testDot() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMagnitude() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testEqualsVector3D() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
