12e4d07f0ac7582274ce0473450f4279f0564f8d
alejandrokatz <n01362083@unf.edu>
2021-10-10 18:25:07 -0400

    test cases

---
+++ PA2/src/cen4010/test/BoardTest.java
@@ -0,0 +1,14 @@
+package cen4010.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ PA2/src/cen4010/test/mainTest.java
@@ -0,0 +1,14 @@
+package cen4010.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class mainTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
