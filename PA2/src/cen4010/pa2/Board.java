9ce87acafc4e8675f7e742609899cef8fc53a287
nhsbi <nhsbi@10.253.62.17>
2021-10-11 16:39:59 -0400

    squares

--- PA2/src/cen4010/pa2/Board.java
+++ PA2/src/cen4010/pa2/Board.java
@@ -2,10 +2,12 @@ package cen4010.pa2;
 
 public class Board {
 	public char boardarray[][];
-	public int size;
-	public Board (int size) {
-		this.boardarray=new char [size][size];
-		this.size=size;
+	public int m;
+	public int n;
+	public Board (int m, int n) {
+		this.boardarray=new char [m][n];
+		this.m=m;
+		this.n=n;
 	}
 	
 	public void playerOneMove(int x, int y) {
--- PA2/src/cen4010/pa2/GUI.java
+++ PA2/src/cen4010/pa2/GUI.java
@@ -9,22 +9,24 @@ JFrame f;
 public GUI() {
 	//tictactoe window
 	f=new JFrame();
-	Board b=new Board(3);
-	JButton [][] buttons= new JButton[b.size][b.size];
+	//f.setLayout(new BorderLayout());
+	Board b=new Board(3,3);
+	JButton [][] buttons= new JButton[b.n][b.m];
 	JPanel p1 = new JPanel();
 	p1.setPreferredSize(new Dimension(400, 400));
 	
 	
-	p1.setLayout(new GridLayout(b.size,b.size));
-	for (int i = 0; i<b.size; i++) {
-		for (int j = 0; j<b.size;j++) {
+	p1.setLayout(new GridLayout(b.n,b.m));
+	for (int i = 0; i<b.n; i++) {
+		for (int j = 0; j<b.m;j++) {
 			buttons[i][j]=new JButton();
-			p1.add(buttons[i][j]);
-			
+			p1.add(buttons[i][j]);			
 		}
 	}
 	
-	f.add(p1);
+	f.add(p1,BorderLayout.CENTER);
+	//f.setBounds(400,400,300,300);	
+	
 	p1.setVisible(true);
 	
 	f.setSize(600,600);//700 width and 500 height  
