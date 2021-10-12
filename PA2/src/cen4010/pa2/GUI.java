05d7d4eb795b435c3e90fb1241669c73febe15aa
Thecodebilly <N01401127@unf.edu>
2021-10-12 00:02:22 -0400

    needed more S P A C E.
    Added spacing and second panel for timer and other operations :-)

--- PA2/src/cen4010/pa2/GUI.java
+++ PA2/src/cen4010/pa2/GUI.java
@@ -1,5 +1,9 @@
 package cen4010.pa2;
-import javax.swing.*;  
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.plaf.DimensionUIResource;
+
+
 import java.awt.*;
 
 
@@ -7,34 +11,69 @@ public class GUI extends JFrame {
 
 JFrame f;
 public GUI() {
-	//tictactoe window
+	//the window
 	f=new JFrame();
-	//f.setLayout(new BorderLayout());
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
+	int height = screenSize.height;
+	int width = screenSize.width;
+	//instantiation of the board object
 	Board b=new Board(3,3);
+	
+	//array of buttons that will be used to place x's and o's
 	JButton [][] buttons= new JButton[b.n][b.m];
+	
+	//this is the array of buttons that makes the visualization of the board 
 	JPanel p1 = new JPanel();
-	p1.setPreferredSize(new Dimension(400, 400));
 	
+	//this sets the frames orientation... 2 rows, 1 column hence (2,1)
+	f.setLayout(new GridLayout(2, 1));
+
+	//sets a grid layout of the panel
+	//a panel is like a small window in a window
+	//set it to mxn size, with small padding in between the buttons
+	p1.setLayout(new GridLayout(b.n,b.m,5,5));
 	
-	p1.setLayout(new GridLayout(b.n,b.m,10,10));
+	//insets buttons into mxn array and puts them in the panel 
 	for (int i = 0; i<b.n; i++) {
 		for (int j = 0; j<b.m;j++) {
 			buttons[i][j]=new JButton();
-			p1.add(buttons[i][j]);			
+			p1.add(buttons[i][j]);	
+			
 		}
 	}
+	//sets border so there is padding at the top and around the sides
+	Border border = p1.getBorder();
+	Border margin = new EmptyBorder(50,10,10,10);
+	p1.setBorder(new CompoundBorder(border, margin));
 	
-	f.add(p1,BorderLayout.CENTER);
-	//f.setBounds(400,400,300,300);	
+	//panel for timer and other accessories
+	JPanel p2 = new JPanel();
 	
+	
+	
+	//that work goes here
+	
+	
+	
+	
+	
+	
+	//sets panels to be visible
 	p1.setVisible(true);
+	p2.setVisible(true);
+	//adds them to main window, and allows them to take up the whole spot that was allocated to them in f's grid layout
+	f.add(p1,BorderLayout.CENTER);
+	f.add(p2,BorderLayout.CENTER);
 	
-	f.setSize(600,600);//700 width and 500 height  
+	//sets size = to 75% of screen size
+	f.setSize((int)(width*.75),(int)(height*.75));
+	//spawns frame in center of the page
+	f.setLocationRelativeTo(null);
 	
+	//making the frame visible  	
 	
-	f.setVisible(true);//making the frame visible  	
-}
-
+	f.setVisible(true);
+	}
 
 
 	  
