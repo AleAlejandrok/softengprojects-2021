bb074f723f019518249bafb2caa76283976ef450
Thecodebilly <N01401127@unf.edu>
2021-10-13 11:19:54 -0400

    added comments

--- PA2/src/cen4010/pa2/GUI.java
+++ PA2/src/cen4010/pa2/GUI.java
@@ -43,7 +43,7 @@ public GUI() {
 			//variables are reassigned to fix scope issue
 			int o=i;
 			int l=j;
-			//button action placeholder
+			//defines a new function that will be performed when the button is pressed
 			ActionListener AL=new ActionListener() {
 		         public void actionPerformed(ActionEvent e) {
 		        	 //if turn is odd, place an x
@@ -55,9 +55,11 @@ public GUI() {
 		         	buttons[o][l].setText("o");
 		         	b.boardarray[o][l]='o';
 		        	 }
+		        	 //turn incrementer
 		         	b.turn_number++;
 		          }
 		       };
+		    //associates the function with the current button in the array 
 			buttons[i][j].addActionListener(AL);
 	}
 	}
@@ -69,7 +71,7 @@ public GUI() {
 	//panel for timer and other accessories
 	JPanel BottomPanel = new JPanel();
 	BottomPanel.setLayout(new GridLayout(2, 1));
-	
+	//creates border spacing
 	Border border2 = BottomPanel.getBorder();
 	Border margin2 = new EmptyBorder(10,150,150,150);
 	BottomPanel.setBorder(new CompoundBorder(border2, margin2));
@@ -78,15 +80,19 @@ public GUI() {
 	JPanel StartGamePanel = new JPanel();
 	StartGamePanel.setLayout(new GridLayout(1, 3, 150,10));
 	
+	//player select 1
 	JComboBox player1type= new JComboBox();
 	StartGamePanel.add(player1type);
 	
+	//used to start timer (maybe?)
 	JButton StartGameButton= new JButton("Start Game");
 	StartGamePanel.add(StartGameButton);
 	
+	//player select 2
 	JComboBox player2type= new JComboBox();
 	StartGamePanel.add(player2type);
 	
+	//add the player select and game start buttons to the bottom half of the screen
 	BottomPanel.add(StartGamePanel);
 	
 	
@@ -95,11 +101,14 @@ public GUI() {
 	TimerPanel.setLayout(new GridLayout(1, 2,50,10));
 	
 	
+	//add timer logic
 	
 	
 	
 	
 	
+	
+	//add the bottom of the second half to the screen
 	BottomPanel.add(TimerPanel);
 	
 	
@@ -107,7 +116,7 @@ public GUI() {
 	
 	
 	
-	//that work goes here
+	
 	
 	
 	
@@ -134,7 +143,7 @@ public GUI() {
 	
 	f.setVisible(true);
 	
-	
+	//end program on close
 	f.addWindowListener(new WindowAdapter() {
         public void windowClosing(WindowEvent windowEvent){
            System.exit(0);
