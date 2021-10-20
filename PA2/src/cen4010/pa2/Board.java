d46c5a6dcc2a783641eb0b017962b7e82dce2d40
Jonathan Shih <jonnyshih@gmail.com>
2021-10-20 14:43:01 -0400

    Added first draft of playerWin and gameDraw functions to board.java
    # WARNING: head commit changed in the meantime
    
    Merge branch 'main' of https://github.com/UNF-CEN4010/T4 into main

--- PA2/src/cen4010/pa2/Board.java
+++ PA2/src/cen4010/pa2/Board.java
@@ -1,25 +1,87 @@
 package cen4010.pa2;
 
 public class Board {
-	public char boardarray[][];
-	public int m;
-	public int n;
+	
+	public static char gameboard[][]; // 2D array game board
+	public static int size; // size of board
+	public static char c; // either x or o mark
+	public int row;
+	public int col;
 	public static int turn_number=1;
-	public Board (int m, int n) {
-		this.boardarray=new char [m][n];
-		this.m=m;
-		this.n=n;
+	
+	public Board (int row, int col) {
+		Board.gameboard = new char [size][size];
+		this.row = size;
+		this.col = size;
 	}
 	
-	
-	
-	//handled by button array
-	public void playerOneMove(int x, int y) {
-		boardarray[x][y]='x';
-	//handled by button array
-	}
-	public void playerTwoMove(int x, int y) {
-		boardarray[x][y]='o';
+	public static boolean playerWin(char[][] board, int[] move, char c) { // checks for winning combinations
+
+	    // check rows
+	    for (int i = 0; i < size; i++) {
+	        if (board[i][0] == c) {
+	            int j;
+
+	            for (j = 1; j < size; j++) {
+	                if (!(board[i][j] == c)) {
+	                    break;
+	                }
+	            }
+
+	            if (j == size) {
+	                return true;
+	            }
+	        }
+	    }
+
+	    // check columns
+	    for (int i = 0; i < size; i++) {
+	        if (board[0][i] == c) {
+	            int j;
+
+	            for (j = 1; j < size; j++) {
+	                if (!(board[j][i] == c)) {
+	                    break;
+	                }
+	            }
+
+	            if (j == size) {
+	                return true;
+	            }
+	        }
+	    }
+
+	    // check diagonals
+	    int i;
+
+	    for (i = 0; i < size; i++) {
+	        if (!(board[i][i] == c)) {
+	            break;
+	        }
+	    }
+
+	    if (i == size) {
+	        return true;
+	    }
+
+	    for (i = 0; i < size; i++) {
+	        if (!(board[i][(size - 1) - i] == c)) {
+	            break;
+	        }
+	    }
+
+	    return i == size;
 	}
 
+	public static boolean gameDraw() { // checks for draws
+	    for (int i = 0; i < size; i++) {
+	        for (int j = 0; j < size; j++) {
+	            if (gameboard[i][j] == 0) {
+	                return false;
+	            }
+	        }
+	    }
+
+	    return true;
+	}
 }
