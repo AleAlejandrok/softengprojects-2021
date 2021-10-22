53ce7a3561ee711247b1a4348563c40367e10979
Jonathan Shih <jonnyshih@gmail.com>
2021-10-22 17:27:28 -0400

    Updated Board.java

--- PA2/src/cen4010/pa2/Board.java
+++ PA2/src/cen4010/pa2/Board.java
@@ -3,84 +3,121 @@ package cen4010.pa2;
 public class Board {
 	
 	public static char gameboard[][]; // 2D array game board
-	public static int size; // size of board
 	public static char c; // either x or o mark
-	public int row;
-	public int col;
+	public static int m; // rows
+	public static int n; // columns
+	public static int k; // score to win
 	public static int turn_number=1;
 	
-	public Board (int row, int col) {
-		Board.gameboard = new char [row][col];
-		this.row = row;
-		this.col = col;
+	public Board (int m, int n) { // board constructor
+		Board.gameboard = new char [m][n];
+		Board.m = m;
+		Board.n = n;
 	}
 	
+	public static void setBoard() { // initializes board to 0s
+	    for (int i = 0; i < m; i++) {
+	        for (int j = 0; j < n; j++) {
+	            Board.gameboard[i][j] = 0;
+	        }
+	    }
+	}
+	
+	public static boolean isValidMove(int x, int y) {
+
+	    // checks if in bounds
+	    if ((x >= m) || (y >= n)) {
+	        return false;
+	    }
+
+	    // checks if space is available
+	    return (gameboard[x][y] == 0);
+	}
+
+	
 	public static boolean playerWin(char[][] board, int[] move, char c) { // checks for winning combinations
-		/*Alejandro's Note: would this method scale upward for later revisions?
-		 * Would it not be prudent to check the for loops vs board.row and board.col now?
-		 * Pardon my ignorance
-		 */
-		
-	    // check rows
-	    for (int i = 0; i < size; i++) {
+
+	    // check horizontal
+	    for (int i = 0; i < m; i++) {
 	        if (board[i][0] == c) {
 	            int j;
 
-	            for (j = 1; j < size; j++) {
+	            for (j = 1; j < n; j++) {
 	                if (!(board[i][j] == c)) {
 	                    break;
 	                }
 	            }
 
-	            if (j == size) {
+	            if (j == k) { // reached required score
 	                return true;
 	            }
 	        }
 	    }
 
-	    // check columns
-	    for (int i = 0; i < size; i++) {
+	    // check vertical
+	    for (int i = 0; i < m; i++) {
 	        if (board[0][i] == c) {
 	            int j;
 
-	            for (j = 1; j < size; j++) {
+	            for (j = 1; j < n; j++) {
 	                if (!(board[j][i] == c)) {
 	                    break;
 	                }
 	            }
 
-	            if (j == size) {
+	            if (j == k) { // reached required score
 	                return true;
 	            }
 	        }
 	    }
-
-	    // check diagonals
-	    int i;
-
-	    for (i = 0; i < size; i++) {
-	        if (!(board[i][i] == c)) {
-	            break;
-	        }
-	    }
-
-	    if (i == size) {
-	        return true;
-	    }
-
-	    for (i = 0; i < size; i++) {
-	        if (!(board[i][(size - 1) - i] == c)) {
-	            break;
-	        }
-	    }
-
-	    return i == size;
+	    checkDiagonals(c);
+	    return false;
 	}
+	
+	
+	public static boolean checkDiagonals(char playerMark) { // checks diagonals for winning condition
 
-	public static boolean gameDraw() { // checks for draws
-	    for (int i = 0; i < size; i++) {
-	        for (int j = 0; j < size; j++) {
-	            if (gameboard[i][j] == 0) { // since char is primitive type can't check for null, instead check if value is zero
+		// running down and right
+		// don't need to iterate rows that can't be the starting point of a winning diagonal formation, thus can exclude some with row < (size - (sizeToWin - 1))
+		for (int row = 0; row < (m - (k - 1)); row++) {
+		    for (int col = 0; col < n; col++) {
+		        int score = 0;
+
+		        // down and right
+		        for (int i = m; i < m; i++) {
+		            if (gameboard[i][i] == 0 ? playerMark == 0 :
+		                    gameboard[i][i] == (playerMark)) {
+		                score++;
+
+		                if (score >= k) {
+		                    return true;
+		                }
+		            }
+		        }
+
+		        score = 0;
+
+		        // down and left
+		        for (int i = row; i < m; i++) {
+		            if (gameboard[i][m - 1 - i] == 0 ? playerMark == 0 :
+		                    gameboard[i][m - 1 - i] == (playerMark)) {
+		                score++;
+
+		                if (score >= k) {
+		                    return true;
+		                }
+		            }
+		        }
+		    }
+		}
+
+		return false;
+		}
+	
+	public static boolean gameDraw() { // checks for draw condition
+	    for (int i = 0; i < m; i++) {
+	        for (int j = 0; j < n; j++) {
+	            if (gameboard[i][j] == 0) {
 	                return false;
 	            }
 	        }
@@ -88,4 +125,5 @@ public class Board {
 
 	    return true;
 	}
+	
 }
