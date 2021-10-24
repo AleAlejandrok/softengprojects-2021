9eb8c431bbc77590dd80ada44dd95ece2bbddc52
NotMexican <alejandro.katz@outlook.com>
2021-10-24 16:30:59 -0400

    testPlayerWind

--- PA2/src/cen4010/test/BoardTest.java
+++ PA2/src/cen4010/test/BoardTest.java
@@ -15,35 +15,18 @@ class BoardTest {
 		
 	}
 
-	@Test
-	void testSetBoard() {
-		Board testBoard = new Board(3,3);
-		
-		int i = 0;
-		int j = 0;
-		for(i = 0; i < 3; i++);
-			for(j = 0; j < 3; j++) {
-				Board.gameboard [i] [j] = 'x';
-				
-			}
-		Board.setBoard();
-		for(i = 0; i < Board.m; i++);
-			for(j = 0; j < Board.n; j++) {
-				if(Board.gameboard [i] [j] != 0)
-					fail();
-			
-		}
-		return;
-	}
 
-	@Test
-	void testIsValidMove() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testPlayerWin() {
-		fail("Not yet implemented");
+		Board testBoard = new Board(3,3);
+		Board.gameboard [0] [1] = 'x';
+		Board.gameboard [0] [2] = 'x';
+		Board.gameboard [0] [3] = 'x';
+		if(Board.playerWin('x'))
+			return;
+		else 
+			fail();
 	}
 
 	@Test
