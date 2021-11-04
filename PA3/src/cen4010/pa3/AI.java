8786ee77ad423f34ac94715dba4e5b53d7ec6e08
Billy Shaw <N01401127@unf.edu>
2021-11-04 15:55:08 -0400

    added pseudo code and constructor for AI

--- PA3/src/cen4010/pa3/AI.java
+++ PA3/src/cen4010/pa3/AI.java
@@ -1,5 +1,48 @@
 package cen4010.pa3;
 
-public class AI {
+/***************************/
+/*HOW WE ARE IMPLEMENTING THE AI*/
+/*
+We will create an AI player that uses a minimax algorithm to train against 
+another instance of the AI class. 
 
-}
+isMaximizingPlayer is something that will be used to denote if the AI is 
+the one that will be playing in the game or if it is the AI used for training.
+
+
+
+*/
+/******************************/
+public class AI {
+	
+	
+	boolean isMaximizingPlayer;
+	
+	public AI (boolean ismax) {
+		this.isMaximizingPlayer=ismax;		
+	}
+	
+	
+	//pseudocode for minimax function
+	/*
+	public minimax(board, depth, isMaximizingPlayer) {
+
+	    if current board state is a terminal state :
+	        return value of the board
+	    
+	    if isMaximizingPlayer :
+	        bestVal = -INFINITY 
+	        for each move in board :
+	            value = minimax(board, depth+1, false)
+	            bestVal = max( bestVal, value) 
+	        return bestVal
+
+	    else :
+	        bestVal = +INFINITY 
+	        for each move in board :
+	            value = minimax(board, depth+1, true)
+	            bestVal = min( bestVal, value) 
+	        return bestVal 
+	}
+	*/
+	}
