c89d1b4e1a30bdff32ce2ab454d08b5a2bb65c21
Thecodebilly <N01401127@unf.edu>
2021-11-14 14:09:37 -0500

    updated player class

--- PA3/src/cen4010/pa3/Player.java
+++ PA3/src/cen4010/pa3/Player.java
@@ -1,11 +1,48 @@
 package cen4010.pa3;
 
+import java.util.Random;
+
 public class Player {
+	public boolean isMaximizingPlayer;
 	boolean isRobot;
-	Player() {
-		this.isRobot = false;
+	
+	
+	Player(boolean robot, boolean isMax) {
+		this.isRobot = robot;
+		isMaximizingPlayer= isMax;
 	}
+	
 	public boolean isRobot() {
 		return this.isRobot;
+	}
+	
+public int[] getMove() {
+		
+		int[] coordinate = new int[2];
+		//get maximums of the board
+		int maxM = Board.m;
+		int maxN = Board.n;
+		//if easy
+		if(!this.isMaximizingPlayer) {
+
+			Random rand = new Random();
+			int randM;
+			int randN;
+			do {
+				randM = rand.nextInt(maxM);
+				randN = rand.nextInt(maxN);
+			}while(Board.gameboard[randM][randN] != 0);
+			
+			coordinate[0] = randM;
+			coordinate[1] = randN;
+			return coordinate;
+		}
+		//else use minmaxxing
+		/**TODO**/
+		else {
+			return coordinate;
+		}
+		
+		
 	}
 }
