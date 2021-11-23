6556cdc75574de74ef71dfffb97e785a51924f83
Thecodebilly <N01401127@unf.edu>
2021-11-23 06:00:08 -0500

    added junit

---
+++ PA4/src/cen4010/test/BoardTest.java
@@ -0,0 +1,44 @@
+package cen4010.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCreateBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlayerWin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGameDraw() {
+		fail("Not yet implemented");
+	}
+
+}
