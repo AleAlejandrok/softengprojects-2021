f2d8ad77854e609a9e6102651addadd41f00924f
Thecodebilly <N01401127@unf.edu>
2021-11-23 05:55:11 -0500

    corrected setup

---
+++ PA4/src/cen4010/pa4/Player.java
@@ -0,0 +1,45 @@
+package cen4010.pa4;
+
+import java.util.Random;
+
+public class Player {
+	
+	boolean isRobot;
+	
+	
+	public Player(boolean robot) {
+		this.isRobot = robot;
+	}
+	
+	public boolean isRobot() {
+		return this.isRobot;
+	}
+	
+public int[] getMove() {
+		
+		int[] coordinate = new int[2];
+		//get maximums of the board
+		int maxM = Board.m;
+		int maxN = Board.n;
+		//if easy
+		
+
+			Random rand = new Random();
+			int randM;
+			int randN;
+			do {
+				randM = rand.nextInt(maxM);
+				randN = rand.nextInt(maxN);
+			}while(Board.gameboard[randM][randN] != 0);
+			
+			coordinate[0] = randM;
+			coordinate[1] = randN;
+			return coordinate;
+		
+		//else use minmaxxing
+		/**TODO**/
+		
+		
+		
+	}
+}
---
+++ PA4/src/cen4010/pa4/tictactoe.java
@@ -0,0 +1,25 @@
+package cen4010.pa4;
+
+public class tictactoe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Board b = new Board(3);
+		int dimmensions[] = null;
+		try {
+			dimmensions = GUI.setUpPopup();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int m = dimmensions[0];
+		int n = dimmensions[1];
+		int k = dimmensions[2];
+		int theme = dimmensions[3];
+		GUI g= new GUI(m,n,k,theme);
+		g.setDefaultCloseOperation(0);
+		
+		
+	}
+
+}
